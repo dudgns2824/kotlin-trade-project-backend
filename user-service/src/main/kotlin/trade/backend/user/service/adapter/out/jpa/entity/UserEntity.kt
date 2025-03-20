1210add52ec5364b70fef268d4376709a93eaa00
dudgns2824 <dudgns2824@gmail.com>
2025-03-20 11:25:13 +0900

    git add

---
+++ user-service/src/main/kotlin/trade/backend/user/service/adapter/out/jpa/entity/UserEntity.kt
@@ -0,0 +1,31 @@
+<<<<<<<< HEAD:user-service/src/main/kotlin/trade/backend/user/service/adapter/out/jpa/entity/UserEntity.kt
+package trade.backend.user.service.adapter.out.jpa.entity
+========
+package trade.backend.user.service.auth.adapter.out.jpa.entity
+>>>>>>>> d2886b4 (user-service gradle 설정 추가):user-service/src/main/kotlin/trade/backend/user/service/auth/adapter/out/jpa/entity/UserEntity.kt
+
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "user")
+data class UserEntity (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idx", nullable = false)
+    var idx: Long? = null,
+    @Column(name = "user_id", nullable = false)
+    var userId: String? = null,
+    @Column(name = "user_name", nullable = false, columnDefinition = "VARCHAR(64)")
+    var userName: String? = null,
+    @Column(name = "user_email", nullable = false, columnDefinition = "VARCHAR(128)")
+    var userEmail: String? = null,
+    @Column(name = "user_password", nullable = false, columnDefinition = "VARCHAR(256)")
+    var userPassword: String? = null,
+    @Column(name = "user_role", nullable = false)
+    var userRole: String? = null,
+    @Column(name = "created_dt", nullable = false, columnDefinition = "DATETIME")
+    @CreationTimestamp
+    private var createdDt: LocalDateTime? = null,
+)
